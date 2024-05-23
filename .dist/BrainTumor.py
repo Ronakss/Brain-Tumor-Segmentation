b7a356d2e7c5e25b4060fcf5f1aa22a864dc8a57
Ronakshakari <shakarironak@yahoo.com>
2024-05-23 21:26:15 +0100

    Create BrainTumor.py

---
+++ .dist/BrainTumor.py
@@ -0,0 +1,8 @@
+# Import necessary packages
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt  # Changed to import pyplot directly
+import os, shutil
+import cv2
+import matplotlib.image as mpimg
+import seaborn as sns
